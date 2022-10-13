19b50933762396ea3a054a09f724095b3d46463a
andres arroyo <aarroyo440@gmail.com>
2022-10-13 10:45:41 -0500

    se agrego al repo

---
+++ model/conexion.php
@@ -0,0 +1,39 @@
+<?php
+
+class conexion {
+
+    private $servidor="b2bryvujgk3mjbrrvj7i-mysql.services.clever-cloud.com";
+    private $usuario="uuyxzjueavnnepg3";
+    private $contrasenia="uuyxzjueavnnepg3";
+    private $conexion;
+
+    public function __construct() {
+
+        try {
+            $this->conexion = new PDO("mysql:host=$this->servidor;dbname=b2bryvujgk3mjbrrvj7i", $this->usuario, $this->contrasenia);
+            $this->conexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        }
+        catch (PDOException $e){
+            return "Falla de conexión".$e;
+        }
+
+    }
+
+    public function ejecutar($sql) {
+
+        $this->conexion->exec($sql);
+        return $this->conexion->lastInsertId();
+
+    }
+
+    public function consultar ($sql) {
+
+        $sentencia = $this->conexion->prepare($sql);
+        $sentencia->execute();
+        return $sentencia->fetchAll();
+
+    }
+
+}
+
+?>
